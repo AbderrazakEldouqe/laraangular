9514318e34e669a0b115d8de8e6f717703bfd003
eldouqe <abderrazak.eldouqe1@gmail.com>
2020-05-09 02:08:53 +0200

    Send Email for Reset Password

--- backend/app/Http/Controllers/ResetPasswordController.php
+++ backend/app/Http/Controllers/ResetPasswordController.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ResetPasswordMail;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
 
 class ResetPasswordController extends Controller
 {
     public function sendEmail(Request $request)
     {
-        return $request->all();
+        if(!$this->validateEmail($request->email))
+        {
+            return $this->failedResponse();
+        }
+        $this->send($request->email);
+        return $this->successResponse();
+    }
+    public function send($email)
+    {
+        Mail::to($email)->send(new ResetPasswordMail);
+    }
+    public function validateEmail($email)
+    {
+        // !! return true or false
+        return !!User::where('email', $email)->first();
+    }
+    public function failedResponse()
+    {
+        return response()->json(
+            [
+            'error'=> 'Email does\'t found on our Database',
+            ],Response::HTTP_NOT_FOUND
+        );
+    }
+    public function successResponse()
+    {
+        return response()->json(
+            [
+            'data'=> 'Reset Email is send successsfully, please check your inbox.',
+            ],Response::HTTP_OK
+        );
     }
 }
